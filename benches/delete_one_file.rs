4870da14aaec479c1065f2788ab633df11a1ae16
Ridwan Hoq <ridwanhoq@gmail.com>
2018-10-08 01:44:40 -0500

    comparison benchmarks against std::fs::remove_dir_all

--- benches/delete_one_file.rs
+++ benches/delete_one_file.rs
@@ -3,30 +3,47 @@ extern crate criterion;
 extern crate nuke;
 
 use criterion::Criterion;
+use criterion::Fun;
+use std::fs;
 use std::path::PathBuf;
 mod utils;
 
 const FILE_SIZE: usize = 500000;
 
-fn delete_one_file_nuke(c: &mut Criterion) {
-    let mut path = PathBuf::from("../delete_one_file/nuke");
+fn delete_one_file(c: &mut Criterion) {
+    let path = write_one_file("../delete_one_file");
+    let nuke_fun = Fun::new("nuke", move |b, p| b.iter(|| nuke::nuke(&p)));
+    let std_fun = Fun::new("std", move |b, p| b.iter(|| fs::remove_dir_all(&p)));
+    let functions = vec!(nuke_fun, std_fun);
+    c.bench_functions("delete_one_file", functions, path);
+}
+
+fn write_one_file(base_path: &str) -> PathBuf {
+    let mut path = PathBuf::from(base_path);
     utils::write_dir(&path);
     path.push("file.txt");
     utils::write_file(FILE_SIZE, &path);
-    c.bench_function("delete_one_file_nuke", move |b| b.iter(|| nuke::nuke(&path)));
+    path
 }
 
-fn delete_one_hundred_files_nuke(c: &mut Criterion) {
-    let path = PathBuf::from("../delete_many_files/nuke");
+fn delete_one_hundred_files(c: &mut Criterion) {
+    let path = write_one_hundred_files("../delete_many_files");
+    let nuke_fun = Fun::new("nuke", move |b, p| b.iter(|| nuke::nuke(&p)));
+    let std_fun = Fun::new("std", move |b, p| b.iter(|| fs::remove_dir_all(&p)));
+    let functions = vec!(nuke_fun, std_fun);
+    c.bench_functions("delete_one_hundred_files", functions, path);
+}
+
+fn write_one_hundred_files(base_path: &str) -> PathBuf {
+    let path = PathBuf::from(base_path);
     utils::write_dir(&path);
     for i in 0..100 {
         let mut file_path = PathBuf::from(&path);
         file_path.push(format!("{}.txt", i));
         utils::write_file(FILE_SIZE, &file_path);
     }
-
-    c.bench_function("delete_one_hundred_files_nuke", move |b| b.iter(|| nuke::nuke(&path)));
+    path
 }
 
-criterion_group!(benches, delete_one_file_nuke, delete_one_hundred_files_nuke);
+criterion_group!(benches, delete_one_file, delete_one_hundred_files);
 criterion_main!(benches);

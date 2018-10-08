b40061a4a551c65779032ee18f69d72513a1e62a
Ridwan Hoq <ridwanhoq@gmail.com>
2018-10-08 01:12:37 -0500

    add delete one hundred files benchmark

--- benches/delete_one_file.rs
+++ benches/delete_one_file.rs
@@ -13,8 +13,20 @@ fn delete_one_file_nuke(c: &mut Criterion) {
     utils::write_dir(&path);
     path.push("file.txt");
     utils::write_file(FILE_SIZE, &path);
-    c.bench_function("nuke one file", move |b| b.iter(|| nuke::nuke(&path)));
+    c.bench_function("delete_one_file_nuke", move |b| b.iter(|| nuke::nuke(&path)));
 }
 
-criterion_group!(benches, delete_one_file_nuke);
+fn delete_one_hundred_files_nuke(c: &mut Criterion) {
+    let path = PathBuf::from("../delete_many_files/nuke");
+    utils::write_dir(&path);
+    for i in 0..100 {
+        let mut file_path = PathBuf::from(&path);
+        file_path.push(format!("{}.txt", i));
+        utils::write_file(FILE_SIZE, &file_path);
+    }
+
+    c.bench_function("delete_one_hundred_files_nuke", move |b| b.iter(|| nuke::nuke(&path)));
+}
+
+criterion_group!(benches, delete_one_file_nuke, delete_one_hundred_files_nuke);
 criterion_main!(benches);
